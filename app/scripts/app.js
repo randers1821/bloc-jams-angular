5872d81f92338b730663b73c3419db351ba66803
Rachel <raedesignz1@gmail.com>
2016-09-30 16:10:49 -0400

    Checkpoint 3 Routing Complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,23 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider   
+            .html5Mode({
+                enabled: true,
+                requiredBase: false
+        });
+        
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+        })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+        });
+    }
+    
+    angular
+        .module('bloc-jams', ['ui.router'])
+        .config(config);
+})();
