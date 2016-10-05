d6de33588bdb2414b6e2c2e583f72b592e8d4617
Rachel <raedesignz1@gmail.com>
2016-10-05 13:50:57 -0400

    Assignment 10 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function () {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
       var SongPlayer = {};
 
       var currentAlbum = Fixtures.getAlbum();
@@ -39,6 +39,12 @@
           preload: true
         });
 
+        currentBuzzObject.bind('timeupdate', function() {
+          $rootScope.$apply(function() {
+            SongPlayer.currentTime = currentBuzzObject.getTime();
+          });
+        });
+
         SongPlayer.currentSong = song;
 
       };
@@ -61,6 +67,13 @@
 */
 
       SongPlayer.currentSong = null;
+
+/**
+* @desc Current playback time (in seconds) of currently playing song
+* @type {Number}
+*/
+
+      SongPlayer.currentTime = null;
 
 /**
 *@function play
@@ -134,6 +147,18 @@
         }
       };
 
+/**
+* @function setCurrentTime
+* @desc Set current time (in seconds) of currently playing song
+* @param {Number} time
+*/
+
+      SongPlayer.setCurrentTime = function(time) {
+        if (currentBuzzObject) {
+          currentBuzzObject.setTime(time);
+        }
+      };
+
       var stopSong = function() {
         currentBuzzObject.stop();
         SongPlayer.currentSong.playing = null;
@@ -150,5 +175,5 @@
 
 angular
   .module('blocJams')
-  .factory('SongPlayer', SongPlayer);
+  .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
